3b291a72269071fbca2dedd5c90e62044be37195
ABDELRAHIM4 <abdulrhimsaeed@gmail.com>
2024-08-12 14:05:45 -0700

    nosql

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -5,13 +5,16 @@ import pymongo
 
 def log_st():
     """Python script that provides some stats"""
-    client = pymongo.MongoClient('mongodb: //127.0.0.1:27017')
+    client = pymongo.MongoClient('mongodb://127.0.0.1:27017')
     Collection = client.logs.nginx
-    docum = Collection.count_documents()
+    docum = Collection.count_documents({})
     print(f"{docum} logs")
     print("Methods")
     for meth in ["GET", "POST", "PUT", "PATCH", "DELETE"]:
-        num = Collection.count_documents({"methods": meth})
+        num = Collection.count_documents({"method": meth})
         print(f"{meth}: {num}")
-    add = Collection.count_documents({ "methods" : "GET", "path": "/status"})
+    add = Collection.count_documents({ "method" : "GET", "path": "/status"})
     print(f"{add} status check")
+
+if __name__ == "__main__":
+    log_st()
