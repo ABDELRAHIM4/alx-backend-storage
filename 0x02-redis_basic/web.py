b4a441a84bf28c0e19a3b74d4d1af410b5eec349
ABDELRAHIM4 <abdulrhimsaeed@gmail.com>
2024-08-16 15:48:05 +0000

    redis

---
+++ 0x02-redis_basic/web.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""will implement a get_page function"""
+import redis
+
+
+def get_page(url: str) -> str:
+    """will implement a get_page function"""
+    use = redis.Redis()
+    count = f"count:{url}"
+    cash = f"cashe:{url}"
+    if use.get(cash):
+        return use.get(cash).decode('utf-8')
+    res = requests.get(url)
+    use.setex(cash, 10, res.txt)
+    use.incr(count)
+    return (res.txt)
